404f5906edc9edd085e274596b2562da3d4590af
Robert Pantea <rpantea@monguz.hu>
2023-11-12 12:26:35 +0100

    foundry v11 compatibility, clear items before adding loot

--- scripts/roll-the-loot.js
+++ scripts/roll-the-loot.js
@@ -1,5 +1,6 @@
 Hooks.on("updateActor", async (actor, update, diff, user) => {
-    if (actor.type !== "npc" || actor.getFlag("core", "sheetClass") === 'dnd5e.LootSheet5eNPC' || update.system.attributes.hp.value > 0)
+    const currentHp = getProperty(update, 'system.attributes.hp.value');
+    if (actor.type !== "npc" || actor.sheet.id.startsWith('LootSheet5eNPC') || currentHp === undefined || currentHp > 0)
         return;
     rollForLoot(actor);
     return true;
@@ -17,11 +18,17 @@ function rollForLoot(actor) {
 function handleRolledItems(actor, roll) {
     const rolledDocumentIds = roll.results.map(element => element.documentId);
     const rolledItems = rolledDocumentIds.map(documentId => game.items.find(item => item._id == documentId));
+    if (rolledItems.length === 0 || rolledItems[0] === undefined) {
+        return;
+    }
+    actor.items.clear();
     rolledItems.forEach(rolledItem => {
-        actor.items.set(rolledItem._id, rolledItem);
+        if(rolledItem !== undefined) {
+            actor.items.set(rolledItem._id, rolledItem);
+        }
     });
     actor.setFlag("core", "sheetClass", "dnd5e.LootSheet5eNPC").then(
-        null,
+        (ret) => console.debug("Sheet Class succesfully set to: " + ret.getFlag("core", "sheetClass")),
         (error) => {console.error(error)}
     );
 }
