b36fc773404107296e99aad1d00e18c5d425ac21
Robert Pantea <rpantea@monguz.hu>
2023-12-20 12:47:09 +0100

    use item-piles to simplify script

--- scripts/roll-the-loot.js
+++ scripts/roll-the-loot.js
@@ -1,34 +1,15 @@
 Hooks.on("updateActor", async (actor, update, diff, user) => {
     const currentHp = getProperty(update, 'system.attributes.hp.value');
-    if (actor.type !== "npc" || actor.sheet.id.startsWith('LootSheet5eNPC') || currentHp === undefined || currentHp > 0)
+    if (actor.type !== "npc" || actor.sheet.id.startsWith('LootSheet5eNPC') || currentHp === undefined || currentHp > 0) {
         return;
-    rollForLoot(actor);
+    }        
+    const actorName = actor.prototypeToken.name
+    await game.itempiles.API.rollItemTable(actorName, {
+        timesToRoll: "1",
+        targetActor: actor,
+        removeExistingActorItems: true
+    });
+    let token = actor.token
+    await game.itempiles.API.turnTokensIntoItemPiles(token);
     return true;
 });
-
-function rollForLoot(actor) {
-    const actorName = actor.prototypeToken.name;
-    const lootTable = game.tables.find(t => t.name === actorName);
-    lootTable.draw({displayChat: false}).then(
-        handleRolledItems.bind(null, actor),
-        (error) => {console.error(error)}
-    );   
-}
-
-function handleRolledItems(actor, roll) {
-    const rolledDocumentIds = roll.results.map(element => element.documentId);
-    const rolledItems = rolledDocumentIds.map(documentId => game.items.find(item => item._id == documentId));
-    if (rolledItems.length === 0 || rolledItems[0] === undefined) {
-        return;
-    }
-    actor.items.clear();
-    rolledItems.forEach(rolledItem => {
-        if(rolledItem !== undefined) {
-            actor.items.set(rolledItem._id, rolledItem);
-        }
-    });
-    actor.setFlag("core", "sheetClass", "dnd5e.LootSheet5eNPC").then(
-        (ret) => console.debug("Sheet Class succesfully set to: " + ret.getFlag("core", "sheetClass")),
-        (error) => {console.error(error)}
-    );
-}
